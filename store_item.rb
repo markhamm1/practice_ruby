f2ab6852b642332a9e4920cac925c79b649df049
Mark <markandrewhamm@gmail.com>
2020-06-22 20:16:30 -0500

    added 3 new items, abc puzzle, jump rope, bee game

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item1 = {name: "ABC Puzzle", price: 5, color: "red", category: "Educational"}
+item2 = {name: "Bee Game", price: 7, color: "yellow", category: "Indoor Fun"}
+item1 = {name: "Jump Rope", price: 3, color: "pink", category: "Outdoor Fun"}
