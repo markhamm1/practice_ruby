7732130a2872d1f67771bf2729b6bb076b8bf9b9
Mark <markandrewhamm@gmail.com>
2020-06-23 19:52:49 -0500

    created file and created Vehicle class

--- store_item.rb
+++ store_item.rb
@@ -1,9 +1,13 @@
 class Item
-  def initialize(name, price, color, category)
-    @name = name
-    @price = price
-    @color = color
-    @category = category
+  
+  attr_reader :active
+  attr_writer :active
+
+  def initialize(input_options)
+    @name = input_options[:name]
+    @price = input_options[:price]
+    @color = input_options[:color]
+    @category = input_options[:category]
   end
 
   def name
@@ -37,14 +41,14 @@ end
 # item2 = {name: "Bee Game", price: 7, color: "yellow", category: "Indoor Fun"}
 # item3 = {:name => "Jump Rope", :price => 3, :color => "pink", :category => "Outdoor Fun"}
 
-item1 = Item.new("ABC Puzzle", 5, "red", "Educational")
+item1 = Item.new({name: "ABC Puzzle", price: 5, color: "red", category: "Educational"})
 item2 = Item.new("Bee Game", 7, "yellow", "Indoor Fun")
 item3 = Item.new("Jump Rope", 3, "pink", "Outdoor Fun")
 
 p item1.name
 
-p item2.get_info
+# p item2.get_info
 
-p item3.price
-item3.new_price = 5
-p item3.price
+# p item3.price
+# item3.new_price = 5
+# p item3.price
