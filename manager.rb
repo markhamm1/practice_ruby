3f201152007504cd96f4a23dbd6b1391ce6518f5
Mark <markandrewhamm@gmail.com>
2020-06-23 21:54:23 -0500

    added give_all_raises and fire_all_employees methods to Manager class

--- manager.rb
+++ manager.rb
@@ -40,19 +40,30 @@ class Manager < Employee
   end
 
   def give_all_raises
-    index = 0
-    while index < @employees.length 
-      @employees[index].give_annual_raise
-      index += 1
+    @employees.each do |employee|
+      employee.give_annual_raise
     end
   end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
+
 end
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 # manager.print_info
 # manager.send_report
 
+p employee1
+p employee2
 
+manager.fire_all_employees
 manager.give_all_raises
 employee1.print_info
 employee2.print_info
+
+p employee1
+p employee2
