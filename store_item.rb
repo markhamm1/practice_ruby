26edd9c61948652cef4e79ad4ffddcc33940ed5d
Mark <markandrewhamm@gmail.com>
2020-06-23 21:15:35 -0500

    added give_all_raises to Manager class

--- store_item.rb
+++ store_item.rb
@@ -36,16 +36,42 @@ class Item
   
 end
 
+class Game < Item
+  # def initialize
+  #   super
+  #   @age_range = input_options[:age_range]
+  # end
+  
+  def fun
+    p "yes"
+  end
+
+  # def age_range
+  #   p @age_range
+  # end
+  
+end
+
+class Food < Item
+  def initialize
+    super
+    @shelf_life = input_options[:shelf_life]
+  end
+end
+
 
 # item1 = {name: "ABC Puzzle", price: 5, color: "red", category: "Educational"}
 # item2 = {name: "Bee Game", price: 7, color: "yellow", category: "Indoor Fun"}
 # item3 = {:name => "Jump Rope", :price => 3, :color => "pink", :category => "Outdoor Fun"}
 
 item1 = Item.new({name: "ABC Puzzle", price: 5, color: "red", category: "Educational"})
-item2 = Item.new("Bee Game", 7, "yellow", "Indoor Fun")
-item3 = Item.new("Jump Rope", 3, "pink", "Outdoor Fun")
+item2 = Game.new(name: "Bee Game", price: 7, color: "yellow", category: "Indoor Fun", age_range: "2-5")
+item3 = Item.new(name: "Jump Rope", price: 3, color: "pink", category: "Outdoor Fun")
+item4 = Food.new(name: "Lollipop", price: 2, color: "rainbow", category: "Food", shelf_life: "2 months")
 
-p item1.name
+item2.fun
+
+# p item1.name
 
 # p item2.get_info
 
