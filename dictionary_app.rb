afac16c22beca94863576ef6b0b3c8fd5ef5acc9
Mark <markandrewhamm@gmail.com>
2020-06-25 20:59:11 -0500

    added top_example and pronunciation. used loop to ensure definition returns a result

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -3,9 +3,31 @@ require 'http'
 puts "Enter a word:"
 user_input = gets.chomp.downcase
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/topExample?useCanonical=false&api_key=")
-
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=") 
 
 definition = response.parse
 
-p definition["text"]
+index = 0
+while true
+  if definition[index]["text"] == nil
+    index += 1
+  else
+    break
+  end
+end
+
+definition = definition[index]["text"]
+
+puts "The definition is '#{definition}'"
+
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/topExample?useCanonical=false&api_key=")
+
+top_example = response.parse["text"]
+
+puts "The top example is '#{top_example}'"
+
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/pronunciations?useCanonical=false&limit=50&api_key=")
+
+pronunciation = response.parse
+
+puts "The pronunciation is '#{pronunciation[0]["raw"]}'"
